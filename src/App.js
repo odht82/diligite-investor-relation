589428a0fdd7f6f2c6169150789ed0d9ac0cf34f
Obaydul Hoque <tusherh82@gmail.com>
2021-10-13 13:00:34 +0600

    recents complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
+import Recents from './Components/Recent/Recents';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Recents></Recents>
       <Footer></Footer>
     </div>
   );
--- src/Components/Recent/Recents.js
+++ src/Components/Recent/Recents.js
@@ -2,9 +2,111 @@ import React from 'react';
 import './Recents.css'
 const Recents = () => {
     return (
-        <div>
-
-        </div>
+        <div className="recents">
+            <div className="recents-container">
+                <h1 className="recnts-heading">
+                    Recents
+                </h1>
+                <div className="rcents-cards">
+                    <div className="result-card">
+                        <div className="heading-container">
+                            <h3 className="card-heading">
+                                Q2-2021
+                            </h3>
+                        </div>
+                        <div className="report-list">
+                            <ul className="row-list">
+                                <li className="rslt-mth">JAN</li>
+                                <li className="rslt-mth">FEB</li>
+                                <li className="rslt-mth">MAR</li>
+                                <li className="rslt-mth">APR</li>
+                            </ul>
+                            <ul className="row-list">
+                                <li className="rslt-mth">MAY</li>
+                                <li className="rslt-mth">JUN</li>
+                                <li className="actv-rslt-mth actv-shadow">JUL</li>
+                                <li className="actv-rslt-mth">AUG</li>
+                            </ul>
+                            <ul className="row-list">
+                                <li className="actv-rslt-mth actv-shadow">SEP</li>
+                                <li className="actv-rslt-mth actv-shadow">OCT</li>
+                                <li className="actv-rslt-mth actv-shadow">NOV</li>
+                                <li className="actv-rslt-mth">DEC</li>
+                            </ul>
+                        </div>
+                        <div className="btn-container">
+                            <button className="see-results-btn">
+                                See Results
+                            </button>
+                        </div>
+                    </div>
+                    <div className="result-card">
+                        <div className="heading-container">
+                            <h3 className="card-heading">
+                                Q2-2021
+                            </h3>
+                        </div>
+                        <div className="report-list">
+                            <ul className="row-list">
+                                <li className="actv-rslt-mth actv-shadow">JAN</li>
+                                <li className="actv-rslt-mth actv-shadow">FEB</li>
+                                <li className="actv-rslt-mth actv-shadow">MAR</li>
+                                <li className="actv-rslt-mth">APR</li>
+                            </ul>
+                            <ul className="row-list">
+                                <li className="actv-rslt-mth actv-shadow">MAY</li>
+                                <li className="actv-rslt-mth">JUN</li>
+                                <li className="rslt-mth">JUL</li>
+                                <li className="rslt-mth">AUG</li>
+                            </ul>
+                            <ul className="row-list">
+                                <li className="rslt-mth">SEP</li>
+                                <li className="rslt-mth">OCT</li>
+                                <li className="rslt-mth">NOV</li>
+                                <li className="rslt-mth">DEC</li>
+                            </ul>
+                        </div>
+                        <div className="btn-container">
+                            <button className="see-results-btn">
+                                See Results
+                            </button>
+                        </div>
+                    </div>
+                    <div className="result-card">
+                        <div className="heading-container">
+                            <h3 className="card-heading">
+                                Q2-2021
+                            </h3>
+                        </div>
+                        <div className="report-list">
+                            <ul className="row-list">
+                                <li className="rslt-mth">JAN</li>
+                                <li className="rslt-mth">FEB</li>
+                                <li className="rslt-mth">MAR</li>
+                                <li className="rslt-mth">APR</li>
+                            </ul>
+                            <ul className="row-list">
+                                <li className="rslt-mth">MAY</li>
+                                <li className="rslt-mth">JUN</li>
+                                <li className="actv-rslt-mth actv-shadow">JUL</li>
+                                <li className="actv-rslt-mth">AUG</li>
+                            </ul>
+                            <ul className="row-list">
+                                <li className="actv-rslt-mth actv-shadow">SEP</li>
+                                <li className="actv-rslt-mth actv-shadow">OCT</li>
+                                <li className="actv-rslt-mth actv-shadow">NOV</li>
+                                <li className="actv-rslt-mth">DEC</li>
+                            </ul>
+                        </div>
+                        <div className="btn-container">
+                            <button className="see-results-btn">
+                                See Results
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 };
 
